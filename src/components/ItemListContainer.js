a20786e2b62f208dae21c29cf3bdb0a1ae80762b
Francisco <Francisco@tests-MacBook-Pro.local>
2022-08-25 21:16:35 -0300

    Version 25.08 probando sincronizacion nuevo repo

--- src/components/ItemListContainer.js
+++ src/components/ItemListContainer.js
@@ -8,6 +8,10 @@ const ItemListContainer = () => {
     <h1 className="text-3xl font-bold underline">
       Hello world!
     </h1>
+
+    <h2 className="text-3xl font-bold underline">
+      Bienvenidos a la prueba!
+    </h2>
      
     </main>
 
