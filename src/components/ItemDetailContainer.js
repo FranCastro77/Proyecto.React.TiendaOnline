b397ac1a24cd572c370fac87e32d49c6d55abb63
Francisco <Francisco@tests-MacBook-Pro.local>
2022-09-06 21:39:52 -0300

    Version luego del after 06.09

--- src/components/ItemDetailContainer.js
+++ src/components/ItemDetailContainer.js
@@ -2,10 +2,21 @@ import {useEffect, useState} from 'react'
 import myProducts from "../components/myProducts.json";
 import { useParams } from 'react-router-dom';
 import ItemDetail from './ItemDetail';
+import {Link} from "react-router-dom";
 
 
 
 const ItemDetailContainer = () => {
+
+  // const getItem = () => { 
+    
+  //    return new Promise((resolve => {
+  //     setTimeout(()=>{
+  //       resolve (myProducts)
+  //     }, 2000);
+  //    }) 
+  // }
+
 
   const {id} = useParams();
 
@@ -18,7 +29,7 @@ const ItemDetailContainer = () => {
 
       setTimeout(() => {
         
-            resolve(myProducts.find((element)=>element.id === id));
+            resolve(myProducts.find((p)=>p.id === id));
         }, 4000)
     }).then((data)=>{
 
@@ -31,11 +42,19 @@ const ItemDetailContainer = () => {
       return (
         <>
     
-       <div>
-        
-          {Object.getOwnPropertyNames(item).length && <ItemDetail item={item}/>}
-        
-        </div>
+      {/* <ul>
+          <li>
+            <NavLink className= "btn btn-active btn-ghost" to={'/Item'}>GoBack</NavLink>
+            {/* <a href = "/#" > Quienes somos </a> */}
+          {/* </li>
+      </ul>   */} 
+
+
+        <Link to={`/Item/:id/${item.id}`} key={item.id}>
+            {item.length ?  item.map((item) => <ItemDetail key={item.id} {...item} />)  : <h3>Estamos por mostrar tu eleccion...</h3>}  
+        {/* {Object.getOwnPropertyNames(item).length && <ItemDetail item={item}/>} */}
+        </Link>
+    
             
         </>
       ) 
