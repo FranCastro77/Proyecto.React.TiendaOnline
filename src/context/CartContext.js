7c9f2c01c42dac05e8932b19c81f69d24436dc00
FranCastro <franciscocast@gmail.com>
2022-09-21 12:05:21 -0300

    Version para entregas 24.09 (con Firebase)

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -10,37 +10,11 @@ export function CartProvider ({children}) {
 
   const [cart, setCart] = useState ([]);
 
-  // let copia = [...cart]
 
-  // console.log("copia", [...copia, 1])
-  
-  // console.log("carrito original", cart)
-
-  
   function isInCart (itemId){
     return cart.find((product) => product.id === itemId);
 
   }
-
-  // function addItem (item, quantity) {
-  //   console.log ({...item, quantity})
-
-  //   if (isInCart(item.id)){
-  //     console.log("este item ya esta agregado al carro")
-  //     let aux = [...cart];
-  //     console.log(aux)
-      
-  //     let itemIndex = aux.findIndex((product) => product.id === item.id);
-  //     console.log(`El index del producto es ${itemIndex}`)
-  //     aux[itemIndex].quantity += quantity;
-  //     setCart([...aux])
-  //   }else{
-  //     console.log(`este artitulo no se registra previamenten en el carro`)
-  //     setCart([...cart, {...item, quantity}]);
-  //   }
-
-  // }
-
 
    function addItem (item, quantity) {
     console.log ({...item, quantity})
